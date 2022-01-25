78e812c0dbbd45e3073515471e766d23db82f15c
abhuiya <anup.bhuiya@gmail.com>
2022-01-25 15:55:54 +0530

    updating commit with latest copy

---
+++ src/test/java/com/azure/demo/rest/services/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.demo.rest.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestfulWebServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
